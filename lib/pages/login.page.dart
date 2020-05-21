bfeba4115aa9106b75a6c9b8ee10b5137db2004e
Thlimass <thlima.silva.souza@gmail.com>
2020-05-21 12:39:51 -0300

    Centralizando texto

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -8,6 +8,21 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, //Largura da coluna (L. pagina)
+            mainAxisAlignment: MainAxisAlignment.center, //Centralizar
+            children: <Widget>[
+              Text('Texto'),
+            ],
+          ),
+        ),
+      ),
+
+    );
   }
 }
